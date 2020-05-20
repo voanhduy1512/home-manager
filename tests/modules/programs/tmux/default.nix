7878ff12f2be33be62cdbdc04256305ee443124c
toonn <toonn@toonn.io>
2020-05-20 15:55:05 +0200

    tmux: Add tests for shortcut and prefix
    Added a test for the new prefix option and one to ensure it doesn't
    break the old shortcut option.

--- tests/modules/programs/tmux/default.nix
+++ tests/modules/programs/tmux/default.nix
@@ -4,4 +4,6 @@
   tmux-vi-all-true = ./vi-all-true.nix;
   tmux-secure-socket-enabled = ./secure-socket-enabled.nix;
   tmux-disable-confirmation-prompt = ./disable-confirmation-prompt.nix;
+  tmux-shortcut-without-prefix = ./shortcut-without-prefix.nix;
+  tmux-prefix = ./prefix.nix;
 }
